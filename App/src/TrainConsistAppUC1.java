d0fe4a6244b26e41e387cf55878ef45757cd61fd
SriNeha06 <srinehavanjavaka@gmail.com>
2026-04-22 11:22:35 +0530

    [SriNeha06]Added[TrainConsistAppUC18]

--- App/src/TrainConsistAppUC1.java
+++ App/src/TrainConsistAppUC1.java
@@ -1,21 +1,31 @@
-import java.util.Arrays;
-
-public class TrainConsistAppUC17 {
+public class TrainConsistAppUC18 {
     public static void main(String[] args) {
         System.out.println("=== Train Consist Management App ===");
 
-        String[] bogieNames = {
-                "Sleeper",
-                "AC Chair",
-                "First Class",
-                "General",
-                "Luxury"
+        String[] bogieIds = {
+                "BG101",
+                "BG205",
+                "BG309",
+                "BG412",
+                "BG550"
         };
 
-        Arrays.sort(bogieNames);
+        String searchKey = "BG309";
 
-        System.out.println("Sorted Bogie Names:");
-        System.out.println(Arrays.toString(bogieNames));
+        boolean found = false;
+
+        for (int i = 0; i < bogieIds.length; i++) {
+            if (bogieIds[i].equals(searchKey)) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Bogie ID " + searchKey + " FOUND in the train consist.");
+        } else {
+            System.out.println("Bogie ID " + searchKey + " NOT FOUND in the train consist.");
+        }
 
         System.out.println("Program continues safely...");
     }
